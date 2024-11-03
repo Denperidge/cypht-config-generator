594179267d2a02bd96aafe679c3294b18138cb6f
Denperidge <cat@denperidge.com>
2024-11-03 13:50:51 +0100

    form.js documentation, cleanup & restructuring

--- form.js
+++ form.js
@@ -1,35 +1,49 @@
-// 1: Get inputs
-function getInput(name) {
-    return $(`[name=${name}]`);
-}
-
-const allInputs = $("input");
-const inputAuthType = getInput("AUTH_TYPE");
-const inputDefaultSmtpName = getInput("DEFAULT_SMTP_NAME");
-const inputAllowLongSession = getInput("ALLOW_LONG_SESSION");
-
-// 2: Helper functions
-function setInputAndRun(jqueryElem, func, checkbox=false) {
-    jqueryElem.on("input", function(e) {
-        let value;
-        if (!checkbox) {
-            value = jqueryElem.val();
-        } else {
-            value = jqueryElem.is(":checked");
-        }
-        func(value)
-    });
-    // TODO: dry
-    let value;
+// Helper functions
+/**
+ * Gets input value, compatible with text/number inputs and checkboxes
+ * 
+ * @param {JQueryElement} jqueryElem what jQuery element to get the value from
+ * @param {Boolean} checkbox whether the jQuery element is a checkbox
+ * @returns {string|Boolean}
+ */
+function getInputValue(jqueryElem, checkbox=false) {
     if (!checkbox) {
-        value = jqueryElem.val();
+        return jqueryElem.val();
     } else {
-        value = jqueryElem.is(":checked");
+        return jqueryElem.is(":checked");
     }
-    func(value);
 }
 
-function ifValueHideOrShow(boolean, jqueryElem) {
+
+// Thanks to https://stackoverflow.com/a/75419102
+/**
+ * Runs provided func with input value as argument when...
+ * - Provided event trigger (default: input)
+ * - Setting the value
+ * 
+ * @param {JQueryElem} jqueryElem what jQuery element to get the value from
+ * @param {(value:String|Boolean) => void} func the function to run on event & run. The input value will be passed as first/only argument
+ * @param {Boolean} checkbox whether the jQuery element is a checkbox
+ * @default false
+ * @param {String} event which event to attach to
+ * @default "input"
+ * 
+ */
+function setInputAndRun(jqueryElem, func, checkbox=false, event="input") {
+    jqueryElem.on(event, function(e) {
+        func(getInputValue(jqueryElem, checkbox));
+    });
+    func(getInputValue(jqueryElem, checkbox));
+}
+
+/**
+ * What it says on the tin: if passed boolean is true,
+ * show passed JQuery elements. Else, hide them.
+ * 
+ * @param {Boolean} boolean whether to show or hide the passed jQuery elements
+ * @param {JQueryElem} jqueryElem which jQuery element(s) to show/hide depending on boolean
+ */
+function ifTrueShowElseHide(boolean, jqueryElem) {
     if (boolean) {
         jqueryElem.show(400);
     } else {
@@ -37,39 +51,47 @@ function ifValueHideOrShow(boolean, jqueryElem) {
     }
 }
 
-// On input
-function onInputAuthType(value) {
-    ifValueHideOrShow(value == "LDAP", $("[id^=LDAP_AUTH_]"));
-    ifValueHideOrShow(value == "IMAP", $("[id^=IMAP_AUTH_]"));    
-}
 
-// This needs a better name
+// This function needs a better name
+/**
+ * From jquery selector string, get the first element.
+ * If the first element/input has value, show the other elements from the selector.
+ * Otherwise, hide them 
+ * 
+ * @param {String} jqueryString - jQuery selector string. `:first` & `:not(:first)`
+ *                                will be appended to select the right elements
+ * 
+ * Note: this function should not be run for things that only
+ * appear in their own module file (e.g. RECAPTCHA, WORDPRESS)
+ */
 function onlyShowRestIfFirstHasValue(jqueryString) {
     const first = $(jqueryString + ":first");
     const otherElements = $(jqueryString + ":not(:first)").parent("fieldset");
 
     setInputAndRun(first, (value) => {
-        ifValueHideOrShow(value.trim() != "", otherElements);
+        ifTrueShowElseHide(value.trim() != "", otherElements);
     })
 }
 
-function onInputDefaultSmtpName(value) {
-    const elems = $("[id^=DEFAULT_SMTP_]:not([id=DEFAULT_SMTP_NAME])")
-    
-    if (value && value.trim() != "") {
-        elems.show(400);
-    } else {
-        elems.hide(400);
-    }
+
+/* Specific events */
+
+// Show related auth fields depending on whether they are selected
+function onInputAuthType(value) {
+    ifTrueShowElseHide(value == "LDAP", $("[id^=LDAP_AUTH_]"));
+    ifTrueShowElseHide(value == "IMAP", $("[id^=IMAP_AUTH_]"));    
 }
 
+// Show LONG_SESSION_LIFETIME when ALLOW_LONG_SESSION is enabled
 function onInputAllowLongSession(value) {
-    ifValueHideOrShow(value == true, $("[id=LONG_SESSION_LIFETIME]"))
+    ifTrueShowElseHide(value == true, $("[id=LONG_SESSION_LIFETIME]"))
 }
 
+// Generate .env file into #result when #generate is clicked
 function onClickGenerate(value) {
     let text = "";
 
+    const allInputs = $("input");
     allInputs.each(function(i) {
         const input = $(this);
         const value = input.attr("type") != "checkbox" ? input.val() : input.is(":checked");
@@ -80,13 +102,12 @@ function onClickGenerate(value) {
     $("#result").val(text)
 }
 
-// Note: the function is not run for things that only appear in their own module file (e.g. RECAPTCHA, WORDPRESS)
+
+/* Main */
 onlyShowRestIfFirstHasValue("[name^=DEFAULT_SMTP_]");
 onlyShowRestIfFirstHasValue("[name^=GMAIL_]");
 onlyShowRestIfFirstHasValue("[name^=OUTLOOK_]");
 
-setInputAndRun(inputAuthType, onInputAuthType);
-setInputAndRun(inputAllowLongSession, onInputAllowLongSession, true);
-
-$("#generate").on("click", onClickGenerate);
-onClickGenerate();
+setInputAndRun($("[name=AUTH_TYPE"), onInputAuthType);
+setInputAndRun($("[name=ALLOW_LONG_SESSION]"), onInputAllowLongSession, true);
+setInputAndRun($("#generate"), onClickGenerate, false, "click")
